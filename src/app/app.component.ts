96db400da1f447456c31ad18da29ee9d93fc13ed
khairil <khairsfwrdev@gmail.com>
2021-02-25 17:34:26 +0800

    add delete function and search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { EmployeeService } from './employee.service';
 export class AppComponent implements OnInit {
   public employees: Employee[];
   public editEmployee: Employee;
+  public deleteEmployee: Employee;
   
   /**
    *
@@ -65,6 +66,35 @@ export class AppComponent implements OnInit {
     );
   }
 
+  onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response:void)=>{
+        console.log(response);
+        this.getEmployees();
+      },
+      (error:HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchEmployees(key: string): void {
+    const results: Employee[] = [];
+    for(const employee of this.employees){
+      if (employee.name.toLowerCase().indexOf(key.toLocaleLowerCase()) !== -1
+     || employee.email.toLowerCase().indexOf(key.toLocaleLowerCase()) !== -1
+     || employee.phone.toLowerCase().indexOf(key.toLocaleLowerCase()) !== -1
+     || employee.jobTitle.toLowerCase().indexOf(key.toLocaleLowerCase()) !== -1
+      ) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length === 0 || !key){
+      this.getEmployees();
+    }
+  }
+
   public onOpenModal(employee: Employee, mode: string): void{
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -79,6 +109,7 @@ export class AppComponent implements OnInit {
     button.setAttribute('data-target','#updateEmployeeModal');
     }
     if(mode === 'delete'){
+      this.deleteEmployee = employee;
     button.setAttribute('data-target','#deleteEmployeeModal');
     }
     container.appendChild(button);
